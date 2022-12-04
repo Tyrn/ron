0d582436391dc9c88f66059b5cacdd212ed89b79
Tyrn <a@b.com>
2022-12-04 11:36:22 +0300

    MDFloatingActionButton example to junk/

---
+++ junk/faction.py
@@ -0,0 +1,48 @@
+import kivy
+
+kivy.require("2.1.0")
+from kivy.utils import platform
+
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivymd.app import MDApp
+from kivymd.uix.tab import MDTabsBase
+from kivymd.icon_definitions import md_icons
+import os
+
+USERPATH = os.path.expanduser("~")
+
+if platform == "android":
+    from android.storage import primary_external_storage_path
+    from android.storage import secondary_external_storage_path
+    from android.permissions import request_permissions, Permission
+
+    USERPATH = primary_external_storage_path()
+    request_permissions(
+        [Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE]
+    )
+
+
+class TabList(FloatLayout, MDTabsBase):
+    """The engaged power supplies tab."""
+
+
+class TabDetails(FloatLayout, MDTabsBase):
+    """The engaged power supply details tab."""
+
+
+class Ron(MDApp):
+    def build(self):
+        return Builder.load_file("faction.kv")
+
+    def on_start(self):
+        self.theme_cls.primary_palette = "Gray"
+        self.theme_cls.material_style = "M3"
+        if platform != "android":
+            self.root.ids.ps_tabs.lock_swiping = True
+
+        self.root.ids.ps_filechooser.rootpath = USERPATH
+
+
+if __name__ == "__main__":
+    Ron().run()
